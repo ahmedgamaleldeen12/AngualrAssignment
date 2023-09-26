cf7c33412baf415bc0c3215fe86a46f8d8a95b06
ahmedgamaleldeen12 <ahmedgamaleldeen12@gmail.com>
2023-09-26 04:31:23 +0300

    second commit

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
-  selectedColor : string = '#2C3E50';
-  portolioTilte :string = 'PORTFOLIO COMPONENT'
+  constructor(private el: ElementRef) { }
+
+
+
+  selectedColor: string = '#2C3E50';
+  portolioTilte: string = 'PORTFOLIO COMPONENT'
+  flagVarToShow: boolean = false;
+  imgSrc: string = ''
+
+  displayImg(url: string) {
+    this.imgSrc = url;
+    this.flagVarToShow = true;
+  }
+  closeImg(event: MouseEvent) {
+    const displayedImgID = this.el.nativeElement.querySelector('#dipalyedImgID');
+    if (this.flagVarToShow && event.target && event.target !== displayedImgID) {
+      this.flagVarToShow = false;
+      this.imgSrc = '';
+    }
+  }
 }
+
+
+
